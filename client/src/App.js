7605b03dbf8ec0805f19756f65efb15edb682390
yusufkaraasln <yusufkaraasln@gmail.com>
2022-05-29 11:57:57 +0300

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { ChatProvider } from './context/ChatContext';
+import Container from './components/Container';
+
+function App() {
+  return (
+    <ChatProvider>
+      <Container></Container>
+    </ChatProvider>
+  );
+}
+
+export default App;
---
+++ client/src/components/ChatForm.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { useChat } from "../context/ChatContext";
+import { sendMessage } from "../socketApi";
+import styles from "./styles.module.css";
+
+function ChatForm() {
+  const [message, setMessage] = useState("");
+  const { setMessages } = useChat();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(message);
+
+    setMessages((prevState) => [...prevState, { message, fromMe:true}]);
+    sendMessage(message);
+    setMessage("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          className={styles.textInput}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default ChatForm;
